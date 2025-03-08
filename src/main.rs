69071977ef2c71fcf79cb9b5914a81b8b783d04b
Koko <neverrare@proton.me>
2025-03-08 18:17:12 +0800

    iterate on all possible places

--- src/main.rs
+++ src/main.rs
@@ -5,34 +5,36 @@ use std::collections::{BinaryHeap, HashMap};
 
 fn main() {
     let domain_regex = Regex::new("^https?://[^/]+/").unwrap();
-    let path = match firefox_places() {
-        Some(path) => path.into_iter().next().unwrap(),
+    let places = match firefox_places() {
+        Some(path) => path,
         None => {
             println!("Firefox not found");
             return;
         }
     };
-    let connection = open(path).unwrap();
-    let rows = connection
-        .prepare("SELECT url, visit_count FROM moz_places")
-        .unwrap();
     let mut map = HashMap::new();
-    for row in rows {
-        let mut row = row.unwrap();
-        let url = match row.take("url") {
-            Value::String(url) => url,
-            _ => panic!("url is not string"),
-        };
-        let domain: Box<str> = match domain_regex.find(&url) {
-            Some(domain) => domain.as_str().into(),
-            None => continue,
-        };
-        let count = match row.take("visit_count") {
-            Value::Integer(count) => count as u64,
-            _ => panic!("visit_count is not integer"),
-        };
-        let rank: &mut u64 = map.entry(domain).or_default();
-        *rank += count;
+    for places in places {
+        let connection = open(places).unwrap();
+        let rows = connection
+            .prepare("SELECT url, visit_count FROM moz_places")
+            .unwrap();
+        for row in rows {
+            let mut row = row.unwrap();
+            let url = match row.take("url") {
+                Value::String(url) => url,
+                _ => panic!("url is not string"),
+            };
+            let domain: Box<str> = match domain_regex.find(&url) {
+                Some(domain) => domain.as_str().into(),
+                None => continue,
+            };
+            let count = match row.take("visit_count") {
+                Value::Integer(count) => count as u64,
+                _ => panic!("visit_count is not integer"),
+            };
+            let rank: &mut u64 = map.entry(domain).or_default();
+            *rank += count;
+        }
     }
     let mut entries: BinaryHeap<_> = map
         .into_iter()
